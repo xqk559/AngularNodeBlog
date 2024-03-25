80e60ad73f18903e9e9f97753d1ea4a8fc54c186
unknown <xqk559@gmail.com>
2024-03-25 15:34:20 -0400

    Repo and first components created

---
+++ src/app/blog-post/blog-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-post.component.html',
+  styleUrl: './blog-post.component.scss'
+})
+export class BlogPostComponent {
+
+}

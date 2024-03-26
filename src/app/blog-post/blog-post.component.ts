8f22044fa56b97cf3c516acde59b40d731afff89
Keith <xqk559@gmail.com>
2024-03-26 15:51:58 -0400

    Commiting Client Submodule

--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from './blog-post.service';
+import { BlogPost } from './blog-post';
 
 @Component({
   selector: 'app-blog-post',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.scss'
 })
-export class BlogPostComponent {
+export class BlogPostComponent implements OnInit {
+  posts: string;
 
+  constructor(private blogPostService: BlogPostService) {}
+
+  ngOnInit() {
+    this.getBlogPosts();
+  }
+
+  getBlogPosts(): void {
+    this.blogPostService.getBlogPost()
+      .subscribe(posts => (this.posts = JSON.stringify(posts)));
+  }
+  //req = http.get<Heroes>('/api/heroes');
+  
+  // configUrl = 'assets/config.json';
+
+  // getConfig() {
+  //   return this.http.get<Config>(this.configUrl);
+  // }
 }
